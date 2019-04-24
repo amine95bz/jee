b8bfd81ffb813467f22e1ffb1f8da4f180fb6a8c
amine95bz <dexter.nlee@gmail.com>
2019-04-24 19:51:15 +0200

    first step

---
+++ ProjJSF2/ProjJSF2-ejb/src/main/java/services/Test.java
@@ -0,0 +1,29 @@
+package services;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateful;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+@Stateful
+@LocalBean
+public class Test implements TestRemote,TestLocale {
+
+	@Override
+	public void Consommation() {
+		// create new jax-rs Client
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:63238/api/RecWebApi");
+		WebTarget hello =target.path("");
+		Response response =hello.request().get();
+		
+		String result=response.readEntity(String.class);
+		System.out.println(result);
+
+		response.close();
+		
+	}
+
+}
